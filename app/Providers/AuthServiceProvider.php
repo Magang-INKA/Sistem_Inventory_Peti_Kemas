13e17eb3e647b728d63b13d8552728fa64585796
Tita Wijayanti <wijayantitita094@gmail.com>
2022-08-08 11:09:29 +0700

    Gate authorization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -10,7 +10,7 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * The policy mappings for the application.
      *
-     * @var array<class-string, class-string>
+     * @var array
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
@@ -25,6 +25,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('manage-MasterData', function($user){
+            return $user->role == "Administrator";
+        });
+
+        Gate::define('manage-transaksi', function($user){
+            return $user->role == "Operator";
+        });
     }
 }
