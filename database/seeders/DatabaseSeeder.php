e3c17f6ac9121e0b1cfb1b88b9b4831109e1ec77
Nadia Layra Aziza <nadialayra15@gmail.com>
2022-08-08 09:50:56 +0700

    Asset inventory & Dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(KategoriSeeder::class);
+        $this->call(BarangSeeder::class);
+        $this->call(SupplierSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box text-white">
+    Email:sdngejugjati2@gmail.com <a href="mailto:sdngejugjati2@gmail.com" >SDN Gejugjati 2</a>
+</div>
