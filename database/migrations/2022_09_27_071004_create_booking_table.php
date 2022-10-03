9dbe82e9f25fc5b19fcded98ef450eca176ab313
Tita Wijayanti <wijayantitita094@gmail.com>
2022-10-03 08:56:44 +0700

    Membuat migration dari pelabuhan, booking, container

---
+++ database/migrations/2022_09_27_071004_create_booking_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('id_barang');
+            $table->foreign('id_barang')->references('id')->on('barang');
+            $table->unsignedBigInteger('id_container');
+            $table->foreign('id_container')->references('id')->on('container');
+            $table->unsignedBigInteger('id_kapal');
+            $table->foreign('id_kapal')->references('id')->on('kapal');
+            $table->unsignedBigInteger('id_pelabuhan');
+            $table->foreign('id_pelabuhan')->references('id')->on('pelabuhan');
+            $table->date('date');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking');
+    }
+}
--- database/migrations/2022_09_29_020004_create_container_table.php
+++ database/migrations/2022_09_29_020004_create_container_table.php
@@ -15,9 +15,11 @@ class CreateContainerTable extends Migration
     {
         Schema::create('container', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('kode_container', 50)->nullable();
+            $table->unsignedBigInteger('id_kapal');
+            $table->foreign('id_kapal')->references('id')->on('kapal');
             $table->string('nama_container', 50)->nullable();
-            $table->text('keterangan')->nullable();
+            $table->unsignedBigInteger('id_pelabuhan');
+            $table->foreign('id_pelabuhan')->references('id')->on('pelabuhan');
             $table->timestamps();
         });
     }
